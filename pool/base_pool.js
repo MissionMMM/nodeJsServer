53f160a6d78fe898cbfb02682cb7c90c73594327
弥森 <2475905645@qq.com>
2024-09-30 15:13:05 +0800

    修改数据库链接

--- pool/base_pool.js
+++ pool/base_pool.js
@@ -2,9 +2,9 @@ const mysql = require('mysql')
 const basePool = mysql.createPool({
     host: '123.58.210.191',
     port:'3306',
-    user: 'admin',
+    user: 'myApp_base_info',
     password: 'yqt123456.',
-    database: 'program_base_info',
+    database: 'myapp_base_info',
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
