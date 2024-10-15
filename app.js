df8a6cf23027813c35b4d2e49a90b142c773a439
弥森 <2475905645@qq.com>
2024-10-15 09:53:44 +0800

    设置中间键

--- app.js
+++ app.js
@@ -1,7 +1,29 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+
+const allowedOrigins = [
+    'http://yqtmisn.top',
+    'http://localhost:3000'
+];
+
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (allowedOrigins.includes(origin) || !origin) { // 不检查null origin（例如，直接从浏览器访问）
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    }, // 指定允许的源
+    optionsSuccessStatus: 200, // 一些浏览器（如IE11）需要一个非标准的成功状态
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // 允许的方法
+    allowedHeaders: ['Content-Type', 'Authorization'] // 允许的头部
+};
+
+// 启用CORS
+app.use(cors(corsOptions));
 
 const usersRouter = require('./routes/users');
 
